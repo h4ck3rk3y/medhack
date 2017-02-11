d4a53c86f0b55ba703d7b9f137938bf9f42cbf17
Gyanendra Mishra <anomaly.the@gmail.com>
2017-02-11 15:13:45 +0800

    gui ready, need to bind with interface

--- arduino.py
+++ arduino.py
@@ -1,64 +1,8 @@
 import serial
 ser = serial.Serial("/dev/ttyACM1", 9600)
 
+import socket
 
-def parse_input():
-
-	current_word = []
-
-	entire_message = []
-
-	abvs = {'btw': 'BY THE WAY', 'hhu': 'HI HOW ARE YOU? ', 'afaik': 'AS FAR AS I KNOW',
-			'pot': 'POINT OF VIEW', 'ge': 'GOOD EVERNING', 'ind': 'INDIA', 'gn': 'GOOD NIGHT',
-			'gm': 'GOOD MORNING',
-			'txt': 'TEXT', 'ie': 'THAT IS', 'brb': 'BE RIGHT BACK', 'cu': 'SEE YOU', 'grt': 'GREAT', 'thnx': 'THANKS', 'lol': 'LAUGHING OUT LOUD', '2': 'TO', '4': 'FOR', 'msg': 'MESSAGE', 'omg': 'OH MY GOD!!', 'asap': 'AS SOON AS POSSIBLE', 'plz': 'PLEASE', 'np': 'NO PROBLEM', '2moro': 'TOMMOROW', 'thku': 'THANK YOU', 'c': 'SEE', 'k': 'OK', 'coz': 'BECAUSE', 'sos': 'SAVE OUR SOULS',
-			'bcc': 'BRING ME A CUP OF COFFEE',
-			'idk': "I DON'T KNOW", 'y': 'WHY', 'ot': 'OUT OF CONTEXT', 'ily': ' I LOVE YOU'}
-
-	while True:
-		data = ser.readline().strip()
-
-
-		if data == "its a dit":
-			current_word.append("0")
-		elif data == "its a dash":
-			current_word.append("1")
-
-		print current_word
-
-		if len(current_word) == 5:
-			number = int(''.join(current_word), 2)
-
-			if not number:
-				# @ToDo Send Message to Rocky
-				message = ''.join(entire_message)
-				if message.strip() in abvs:
-					message = abvs[message.strip()].lower()
-					message[0] = message[0].upper()
-					print message
-				else:
-					print 'Entire Message: %s' %(message)
-				entire_message = []
-				current_word = []
-				continue
-			elif number < 26:
-				message = chr(ord('a') + number - 1)
-				print 'Character: %s' %(message)
-			elif number == 27:
-				message = " "
-			elif number == 28:
-				# @ToDO Gyani independent send this asap
-				message = "change_mode"
-				current_word = []
-				continue
-			elif number == 29:
-				# @ToDO independent send
-				message = "this_is_an_sos"
-				current_word = []
-				continue
-
-			entire_message.append(message)
-			current_word = []
 
 
 
--- videoplayer.py
+++ videoplayer.py
@@ -1,30 +1,17 @@
+
 #! /usr/bin/python
 
-#
-# gtk example/widget for VLC Python bindings
-# Copyright (C) 2009-2010 the VideoLAN team
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
-#
-
 """VLC Gtk Widget classes + example application.
+
 This module provides two helper classes, to ease the embedding of a
 VLC component inside a pygtk application.
+
 VLCWidget is a simple VLC widget.
+
 DecoratedVLCWidget provides simple player controls.
+
 When called as an application, it behaves as a video player.
+
 $Id$
 """
 
@@ -36,11 +23,21 @@ import vlc
 
 from gettext import gettext as _
 
+def send_message(message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ip='192.168.43.132'
+    port = 8080
+    BUFFER_SIZE = 1024
+    sock.connect((ip, port))
+    sock.send(message)
+    sock.close()
+
 # Create a single vlc.Instance() to be shared by (possible) multiple players.
 instance = vlc.Instance()
 
 class VLCWidget(gtk.DrawingArea):
     """Simple VLC widget.
+
     Its player can be controlled through the 'player' attribute, which
     is a vlc.MediaPlayer() instance.
     """
@@ -56,9 +53,166 @@ class VLCWidget(gtk.DrawingArea):
         self.connect("map", handle_embed)
         self.set_size_request(320, 200)
 
+
+
+class HackPad(gtk.VBox):
+
+    def __init__(self, *p):
+        gtk.VBox.__init__(self)
+
+
+
+class Loco(gtk.VBox):
+
+    def __init__(self, *p):
+        gtk.VBox.__init__(self)
+
+        self.up = gtk.Button("Up")
+        self.dw = gtk.Button("Dw")
+        self.le = gtk.Button("Dw")
+        self.ri = gtk.Button("Ri")
+        self.st = gtk.Button("st")
+
+        self.up.connect("clicked", self.upc)
+        self.dw.connect("clicked", self.dwc)
+        self.le.connect("clicked", self.lec)
+        self.ri.connect("clicked", self.ric)
+        self.st.connect("clicked", self.stc)
+
+        self.add(self.up)
+        self.add(self.dw)
+        self.add(self.le)
+        self.add(self.ri)
+        self.add(self.st)
+
+        self.st.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#00ff00'))
+
+    def upc(self, p):
+        self.up.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#00ff00'))
+        self.dw.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.le.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.ri.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.st.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+
+
+    def dwc(self, p):
+        self.up.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.dw.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#00ff00'))
+        self.le.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.ri.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.st.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+
+    def lec(self, p):
+        self.up.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.dw.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.le.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#00ff00'))
+        self.ri.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.st.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+
+    def ric(self, p):
+        self.up.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.dw.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.le.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.ri.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#00ff00'))
+        self.st.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+
+
+
+
+    def stc(self, p):
+        self.up.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.dw.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.le.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.ri.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#000000'))
+        self.st.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#00ff00'))
+
+
+class Talk(gtk.VBox):
+
+    def __init__(self, *p):
+        gtk.VBox.__init__(self)
+
+        self.message = gtk.TextView(buffer=None)
+
+        self.input = gtk.TextView(buffer=None)
+
+        self.input.set_editable(True)
+        self.send = gtk.Button("Send Input")
+
+        self.message.set_size_request(30,30)
+        self.input.set_size_request(30,30)
+
+        self.add(self.message)
+        self.add(self.input)
+        self.add(self.send)
+
+
+class Top(gtk.HBox):
+
+    def __init__(self, outer):
+        gtk.HBox.__init__(self)
+
+        self.outer = outer
+
+        self.Loco = gtk.Button("Loco")
+
+        self.Loco.connect("clicked", self.locob)
+
+        self.Loco.set_size_request(100,20)
+
+        self.Talk = gtk.Button("Talk")
+
+        self.Talk.connect("clicked", self.talkb)
+
+        self.Talk.set_size_request(100,20)
+
+
+        self.pack_start(self.Loco, expand=False, padding=20)
+        self.pack_start(self.Talk, expand=False, padding=0)
+
+
+    def locob(self, p=None):
+        if self.outer.talk in self.outer:
+            self.outer.remove(self.outer.talk)
+        self.outer.add(self.outer.loco)
+        self.Loco.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#00ff00'))
+        self.Talk.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#ff0000'))
+
+    def talkb(self, p=None):
+        if self.outer.loco in self.outer:
+            self.outer.remove(self.outer.loco)
+
+        self.outer.add(self.outer.talk)
+
+        self.Loco.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#ff0000'))
+        self.Talk.modify_bg(gtk.STATE_NORMAL,gtk.gdk.color_parse('#00ff00'))
+
+
+
+class Controller(gtk.VBox):
+
+    def __init__(self, *p):
+        gtk.VBox.__init__(self)
+
+
+        self.loco = Loco()
+        self.talk = Talk()
+
+        self.top = Top(self)
+
+        self.add(self.top)
+        self.add(HackPad())
+        self.add(HackPad())
+
+        self.add(self.loco)
+        self.add(self.talk)
+
+
 class DecoratedVLCWidget(gtk.VBox):
     """Decorated VLC widget.
+
     VLC widget decorated with a player control toolbar.
+
     Its player can be controlled through the 'player' attribute, which
     is a Player instance.
     """
@@ -103,28 +257,30 @@ class VideoPlayer:
 
 class MultiVideoPlayer:
     """Example multi-video player.
+
     It plays multiple files side-by-side, with per-view and global controls.
     """
     def main(self, filenames):
         # Build main window
         window=gtk.Window()
-        mainbox=gtk.VBox()
-
+        mainbox=gtk.HBox()
         videos=gtk.HBox()
 
+        controls = gtk.HBox()
+
         window.add(mainbox)
-        mainbox.add(controls)
         mainbox.add(videos)
+        mainbox.add(controls)
+
+        control_box = Controller()
+        controls.add(control_box)
+
 
         # Create VLC widgets
         for fname in filenames:
             v = DecoratedVLCWidget()
             v.player.set_media(instance.media_new(fname))
             videos.add(v)
-
-        # Create global toolbar
-        tb = gtk.Toolbar()
-        tb.set_style(gtk.TOOLBAR_ICONS)
 
         def execute(b, methodname):
             """Execute the given method on all VLC widgets.
@@ -133,31 +289,14 @@ class MultiVideoPlayer:
                 getattr(v.player, methodname)()
             return True
 
-        for text, tooltip, stock, callback, arg in (
-            (_("Play"), _("Global play"), gtk.STOCK_MEDIA_PLAY, execute, "play"),
-            (_("Pause"), _("Global pause"), gtk.STOCK_MEDIA_PAUSE, execute, "pause"),
-            (_("Stop"), _("Global stop"), gtk.STOCK_MEDIA_STOP, execute, "stop"),
-            ):
-            b = gtk.ToolButton(stock)
-            b.set_tooltip_text(tooltip)
-            b.connect("clicked", callback, arg)
-            tb.insert(b, -1)
-
-        mainbox.pack_start(tb, expand=False)
-
         window.show_all()
         window.connect("destroy", gtk.main_quit)
+
+        control_box.top.locob()
+
         gtk.main()
 
 if __name__ == '__main__':
-    if not sys.argv[1:]:
-       print "You must provide at least 1 movie filename"
-       sys.exit(1)
-    if len(sys.argv[1:]) == 1:
-        # Only 1 file. Simple interface
-        p=VideoPlayer()
-        p.main(sys.argv[1])
-    else:
-        # Multiple files.
-        p=MultiVideoPlayer()
-        p.main(sys.argv[1:])
+    # Multiple files.
+    p=MultiVideoPlayer()
+    p.main(["do.mp4"])
